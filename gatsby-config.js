e04bcc0b20f1c3bd27908b7d0973675cb0dc3246
cory <corymcdaniel@gmail.com>
2020-05-07 17:03:18 -0400

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `corymcdaniel.com`,
+    description: `Site for www.corymcdaniel.com.`,
+    author: `Cory McDaniel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,18 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`postcss-preset-env`)({ stage: 0 }),
+          require('postcss-functions')(),
+          require('postcss-import')(),
+          require('postcss-simple-vars')(),
+          require('postcss-nested')(),
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
