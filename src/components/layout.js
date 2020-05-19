6436763c2eb6763c001c07a298240970cf29ccab
Cory <corymcdaniel@gmail.com>
2020-05-19 14:13:44 -0400

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,8 +28,7 @@ const Layout = ({ children }) => {
         <footer className={footer}>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a> -- a bitchin' static site generator
-          utilizing React
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
